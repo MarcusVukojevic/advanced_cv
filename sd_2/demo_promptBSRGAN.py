366556780734696ec7b2a56f4fb5bf0051995c9c
Carlotta Giacchetta <85078090+CarlottaGiacchetta@users.noreply.github.com>
2024-11-16 11:16:40 +0100

    jsakja

--- sd_2/demo_promptBSRGAN.py
+++ sd_2/demo_promptBSRGAN.py
@@ -23,15 +23,15 @@ if __name__ == "__main__":
     model_gpt_name = "gpt-4o-mini"
 
     # Caricamento immagine
-    img = processor.load_and_preprocess_image(input_image_path, 512, 512)
-    img_np = np.array(img)
+    img_iniziale = processor.load_and_preprocess_image(input_image_path, 512, 512)
+    img_np = np.array(img_iniziale)
 
     # Applica BSRGAN
     img_bsrgan = processor.apply_bsrgan(img_np, bsrgan_model_path)
 
     # Suddividi in patch
     patches = processor.get_views(img_bsrgan, window_size=128, stride=64)
-
+    
     # Processa le patch
     processed_patches = processor.process_patches(patches, model_gpt_key, model_gpt_name, prompt="image quality")
 
@@ -39,7 +39,20 @@ if __name__ == "__main__":
     reassembled_image = processor.assemble_patches(processed_patches, img_bsrgan.shape, window_size=128, stride=64)
 
     # Salva e mostra
-    Image.fromarray(reassembled_image).save("reassembled_image.png")
-    plt.imshow(reassembled_image)
-    plt.axis("off")
-    plt.show()
+    img = Image.fromarray(reassembled_image)
+    img.save("reassembled_image.png")
+
+    fig, axes = plt.subplots(1, 3, figsize=(10, 10))
+    axes = axes.ravel()  # Appiattisce l'array per accedere agli assi più facilmente
+    axes[0].imshow(img_iniziale)
+    axes[0].set_title(f"Immagine Iniziale Piccola")
+    axes[0].axis('off')
+    axes[1].imshow(img_bsrgan)
+    axes[1].set_title(f"Immagine Iniziale Ingrandita")
+    axes[1].axis('off')
+    axes[2].imshow(img)
+    axes[2].set_title(f"Immagine Ingrandita Migliorata")
+    axes[2].axis('off')
+    plt.tight_layout()
+    plt.show() 
+
