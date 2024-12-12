84454b54c8b473fbf2997087be0705fdc1a41129
Carlotta Giacchetta <carlotta.giacchetta@outlook.it>
2024-12-12 15:05:10 +0100

    jqwdnc

--- DemoFusion2.0(consegna)/GoodPromptBSRGAN.py
+++ DemoFusion2.0(consegna)/GoodPromptBSRGAN.py
@@ -1,16 +1,14 @@
-import os
 import torch
 import random
 import numpy as np
 from PIL import Image
-#import matplotlib.pyplot as plt
 from network_rrdbnet import RRDBNet as net
 import utils_image as util
 from transformers import CLIPTokenizer
-from openai import OpenAI
-import pipeline
+import pipeline_sd
 import model_loader
 from concurrent.futures import ThreadPoolExecutor
+import matplotlib as plt
 
 
 class ImageProcessor:
@@ -43,6 +41,7 @@ class ImageProcessor:
         self._set_device()
         self.model = model_loader.preload_models_from_standard_weights("data/v1-5-pruned-emaonly.ckpt", self.device)
         self.tokenizer = CLIPTokenizer("data/tokenizer_vocab.json", merges_file="data/tokenizer_merges.txt")
+        self.model_file = "../data/v1-5-pruned-emaonly.ckpt"
         self.patches_pil=[]
 
     def _set_device(self):
@@ -372,7 +371,7 @@ class ImageProcessor:
         num_inference_steps = 50
         seed = 42
 
-        output_image = pipeline.generate(
+        output_image = pipeline_sd.generate(
             prompt='Smooth and higher quality image with enhanced details' + description,
             uncond_prompt="",
             input_image=image,
@@ -485,5 +484,86 @@ class ImageProcessor:
         with ThreadPoolExecutor(max_workers=num_parallel) as executor:
             processed_patches = list(executor.map(self.process_single_patch, range(len(patches))))
         return processed_patches
+    
+
+    def FlashSDL(self, input_image_path=None, prompt=None, uncond_prompt=None, do_cfg=True, cfg_scale=8, window_size=512, stride=362, bsrgan_time=2, parallel=1):
+        bsrgan_model_path = "data/BSRGANx2.pth"
+
+        # Caricamento immagine
+        if prompt is not None:
+            models = self.model 
+            tokenizer = self.tokenizer 
+            #model_file = self.model_file 
+            output_image = pipeline_sd.generate(
+                prompt=prompt,
+                uncond_prompt=uncond_prompt,
+                input_image=None,
+                strength=0.25,
+                do_cfg=do_cfg,
+                cfg_scale=cfg_scale,
+                sampler_name="ddpm",
+                n_inference_steps=50,
+                seed=42,
+                models=models,
+                device=self.device,
+                idle_device=self.device,
+                tokenizer=tokenizer,
+            )
+            output_image.save(f"{input_image_path}")
+
+
+        img_iniziale = self.load_and_preprocess_image(input_image_path)
+        img_np = np.array(img_iniziale, dtype=np.uint8)
+        print("dimensione dell'immagine iniziale:",img_np.shape)
+        # Applica BSRGAN
+        for i in range(bsrgan_time):
+            print(i)
+            img_bsrgan = self.apply_bsrgan(img_np, bsrgan_model_path)
+        print("dimensione dell'immagine finale:",img_bsrgan.shape)
+        img = Image.fromarray(img_bsrgan)
+        img.save(f"{input_image_path}_bsrgan")
+        print(f'immagine ingrandita salvata in {input_image_path}_bsrgan')
+
+        # Suddividi in patch
+        patches, positions  = self.get_views(img_bsrgan, window_size=window_size, stride=stride, random_jitter=False, vae_scale_factor=1)
+        print('numero patches: ',len(patches))
+        for patch in patches:
+            if patch.shape != (512, 512, 3):
+                print('-- error: HO UN ERRORE DI DIMENSIONE IN UN PATCH, shape:',patch.shape)
+        
+        # Processa le patch
+        if parallel > 1:
+            processed_patches = self.process_patches_parallel(patches, num_parallel=parallel)
+        else:
+            processed_patches = self.process_patches(patches)
+
+        # Ricomponi l'immagine
+        reassembled_image = self.assemble_patches(patches=processed_patches, positions=positions, image_shape=img_bsrgan.shape, window_size=window_size, stride=stride)
+        # Salva e mostra
+        img = Image.fromarray(reassembled_image)
+        img.save(f"{input_image_path}_finale")
+        print(f'immagine finale migliorata salvata in {input_image_path}_finale')
+        fig, axes = plt.subplots(1, 3, figsize=(10, 10))
+        axes = axes.ravel()  # Appiattisce l'array per accedere agli assi più facilmente
+        axes[0].imshow(img_iniziale)
+        axes[0].set_title(f"Immagine Iniziale Piccola")
+        axes[0].axis('off')
+        axes[1].imshow(img_bsrgan)
+        axes[1].set_title(f"Immagine Iniziale Ingrandita")
+        axes[1].axis('off')
+        axes[2].imshow(img)
+        axes[2].set_title(f"Immagine Ingrandita Migliorata")
+        axes[2].axis('off')
+        plt.tight_layout()
+        plt.show() 
+
+
+
+
+        
+
+
+
+
 
 
