d8f0bfe771edf76158f0e1ebc34932f02d7fc099
Carlotta Giacchetta <carlotta.giacchetta@outlook.it>
2024-12-14 12:52:40 +0100

    djhihooooo

--- FlashSD/demo.py
+++ FlashSD/demo.py
@@ -6,12 +6,12 @@ from GoodPromptBSRGAN import ImageProcessor
 # Esempio di utilizzo della classe
 if __name__ == "__main__":
 
-    prompt='image of a little beatiful fox in the nature with a lot of furr, centered in the image immerse in the nature'
+    prompt=None
     processor = ImageProcessor(device='mps')
     window_size_param = 512
     stride_param = 362
 
-    input_image_path = "immagini/foxy_2.png"
+    input_image_path = "immagini/teddy/teddyBear.png"
 
     processor.FlashSDL(input_image_path=input_image_path,prompt=prompt, bsrgan_time=2, parallel=1)
 
